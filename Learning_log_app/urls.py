1b1b20381dfa61d750864a8fff65ffe10479eebd
musawakiliML <musawakiliml@hotmail.com>
2022-08-28 23:31:56 +0100

    Added home page urls, view and base views

--- Learning_log_app/urls.py
+++ Learning_log_app/urls.py
@@ -1,9 +1,13 @@
 """Defines URL patterns for the Learning_log_app"""
 from django.urls import path
-from .views import index
+from .views import index, homepage
 
 app_name = "Learning_log_app"
 urlpatterns = [
-    # Home page
-    path('', index, name='index'),
+    # Parent Home page
+    path('', homepage, name='homepage'),
+
+    # App Home page
+    path('index/', index, name='index'),
+
 ]
--- Learning_log_app/views.py
+++ Learning_log_app/views.py
@@ -1,4 +1,6 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -6,3 +8,16 @@ from django.shortcuts import render
 def index(request):
     """The home page for the learning log app"""
     return render(request, 'index.html')
+
+
+def homepage(request):
+    """Return Parent Homepage"""
+
+    return render(request, 'base.html')
+
+
+def topics(request):
+    """Return Topics on the home page"""
+    topics = Topic.objects.all
+    context = {'topics': topics}
+    return render(request, 'topics.html', context)
