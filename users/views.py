444cc94134758f8b044afb4f9c314f2571957b27
musawakiliML <musawakiliml@hotmail.com>
2022-08-30 21:16:21 +0100

    Added register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register(request):
+    """Register a New User"""
+    if request.method != "POST":
+        # Display blank registration form
+        form = UserCreationForm()
+    else:
+        # Process completed form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Login redirect to home page
+            login(request, new_user)
+            return redirect('Learning_log_app:index')
+
+    # Display blank or invalid form
+    context = {'form': form}
+    return render(request, 'register.html', context)
