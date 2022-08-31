d97381f8e0b8de32538ff987ec4ba681e18aedab
musawakiliML <musawakiliml@hotmail.com>
2022-08-31 09:52:16 +0100

    Made makemigartions adding topic

--- Learning_log_app/models.py
+++ Learning_log_app/models.py
@@ -1,4 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,6 +16,7 @@ class Topic(models.Model):
     """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         """Return a string representation of the model."""
